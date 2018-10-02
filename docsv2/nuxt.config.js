4ec459fe6d8483f358c90756674a7274f7abea6b
Afif Sohaili <afifnajib@gmail.com>
2018-10-02 10:33:34 +0800

    [DOCS-SSR] Add CodeView, add pre filter and highlight directive.
    Add back highlight.js to nuxt dependency.

--- docsv2/nuxt.config.js
+++ docsv2/nuxt.config.js
@@ -34,7 +34,10 @@ module.exports = {
      ** Plugins to load before mounting the App
      */
     plugins: [
-        '~/plugins/buefy'
+        '~/plugins/buefy',
+        '~/plugins/code-view',
+        '~/plugins/highlight',
+        '~/plugins/pre'
     ],
 
     /*
---
+++ docsv2/plugins/code-view.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import CodeView from '~/components/CodeView'
+
+Vue.component('CodeView', CodeView)
